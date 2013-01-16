5f77f65e7eee6e3c588820949b77c088f68d57fa
Dave Louw <developer@slobrewer.com>
2013-01-16 00:12:43 -0800

    FERM-31: Added code to pull temps from 1-wire files

--- fermbot/thermo.py
+++ fermbot/thermo.py
@@ -1,8 +1,6 @@
-'''
-Created on Jan 13, 2013
+import os
 
-@author: dlouw
-'''
+from decimal import Decimal
 
 RPI_BUS_PATH = "/sys/devices/w1_bus_master1"
 
@@ -10,18 +8,87 @@ DS18B20_1_WIRE_TYPE = 0x28
 DS18B20_1_WIRE_TYPE_PREFIX = hex(DS18B20_1_WIRE_TYPE).replace("0x", "", 1)
 
 SLAVE_LIST_FILE_PATH = "/w1_master_slaves"
+TEMPERATURE_FILE_PATH = "/w1_slave"
 
-class Thermometer:
-    def __init__(self, serial):
+class Thermometer(object):
+    def __init__(self, bus_master_path, serial):
+        self.bus_master_path = bus_master_path
         self.serial = serial
-        
-    serial = ""
     
+    # Serial property: the unique 1-wire serial number for the thermometer    
+    _serial = ""
+    
+    @property
+    def serial(self):
+        return self._serial
+    
+    @serial.setter
+    def serial(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Serial must be a string")
+        if not value.startswith(DS18B20_1_WIRE_TYPE_PREFIX + "-"):
+            raise ValueError("Only type " + DS18B20_1_WIRE_TYPE_PREFIX +
+                             " 1-wire devices are supported.")
+        self._serial = value
+    
+    # Bus Master Path property: the file system path to the 1-wire bus master
+    _bus_master_path = ""
+    
+    @property
+    def bus_master_path(self):
+        return self._bus_master_path
+    
+    @bus_master_path.setter
+    def bus_master_path(self, value):
+        if not os.access(value, os.F_OK):
+            raise ValueError("bus_master_path '" + value +
+                             "' doesn't point to a directory")
+        
+        if not value.endswith("/"):
+            value += "/"
+        
+        self._bus_master_path = value
+        pass
+    
+    # Temperature in Celcius property: provides the current temperature in
+    # degrees Celcius by querying the 1-wire bus files.  Precision is to the
+    # 1000th of a degree but accuracy is only to +/-0.5 C.
+    @property
+    def temp_c(self):
+        with open(self.bus_master_path + self.serial +
+                  TEMPERATURE_FILE_PATH) as temperature_file:
+            crc_line = temperature_file.readline();
+            temp_line = temperature_file.readline();
+            temperature_file.close()
+
+            crc = crc_line.split()[-1]
+
+            if crc.split()[-0] == "NO":
+                raise TempReadingError("Bad reading from thermometer '" +
+                                       self.serial + "'")
+            
+            temperature_data = temp_line.split()[-1]
+            return (Decimal(temperature_data[2:]) /
+                Decimal(1000))
+    
+    # Temperature in Fahrenheit property: provides the current temperature in
+    # degrees Farenheit by querying the 1-wire bus files.  Precision is to the
+    # 1000th of a degree but accuracy is only to +/-0.5 C.
+    @property
+    def temp_f(self):
+        return ((self.temp_c * Decimal(9) / Decimal(5)) +
+                Decimal(32)).quantize(Decimal('1.000'))
+
+class TempReadingError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
 def get_thermometers(bus_master_path):
     thermometers = []
     with open(bus_master_path + SLAVE_LIST_FILE_PATH) as w1_master_slaves_file:
         for slave in w1_master_slaves_file:
             if slave.startswith(DS18B20_1_WIRE_TYPE_PREFIX):
-                thermometers.append(Thermometer(slave.strip()))
+                thermometers.append(Thermometer(bus_master_path,
+                                                slave.strip()))
                 
     return thermometers
--- tests/test_thermo.py
+++ tests/test_thermo.py
@@ -1,8 +1,10 @@
 import pytest
 import fermbot.thermo
+import decimal
 
 SINGLE_THERMO_BUS_PATH = "data/thermo/single_thermo_bus_master"
 DUAL_THERMO_BUS_PATH = "data/thermo/dual_thermo_bus_master"
+BAD_CRC_THERMO_BUS_PATH = "data/thermo/bad_crc_thermo_bus_master"
 
 def test_get_thermometers_exactly_one():
     assert len(fermbot.thermo.get_thermometers(SINGLE_THERMO_BUS_PATH)) == 1
@@ -20,3 +22,31 @@ def test_get_thermometers_check_names_double():
     assert len(fermbot.thermo.get_thermometers(DUAL_THERMO_BUS_PATH)) == 2
     assert thermometers[0].serial == "28-0000041481e8"
     assert thermometers[1].serial == "28-0000041462fa"
+    
+def test_thermometer_temp_c():
+    thermometers = fermbot.thermo.get_thermometers(SINGLE_THERMO_BUS_PATH)
+    assert thermometers[0].temp_c == decimal.Decimal("19.562")
+    
+def test_thermometer_multiple_temp_c():
+    thermometers = fermbot.thermo.get_thermometers(DUAL_THERMO_BUS_PATH)
+    assert thermometers[0].temp_c == decimal.Decimal("19.562")
+    assert thermometers[1].temp_c == decimal.Decimal("18.125")
+        
+def test_thermometer_temp_c_bad_crc():
+    thermometers = fermbot.thermo.get_thermometers(BAD_CRC_THERMO_BUS_PATH)
+    with pytest.raises(fermbot.thermo.TempReadingError): #@UndefinedVariable
+        thermometers[0].temp_c
+    
+def test_thermometer_temp_f():
+    thermometers = fermbot.thermo.get_thermometers(SINGLE_THERMO_BUS_PATH)
+    assert thermometers[0].temp_f == decimal.Decimal("67.212")
+    
+def test_thermometer_multiple_temp_f():
+    thermometers = fermbot.thermo.get_thermometers(DUAL_THERMO_BUS_PATH)
+    assert thermometers[0].temp_f == decimal.Decimal("67.212")
+    assert thermometers[1].temp_f == decimal.Decimal("64.625")
+        
+def test_thermometer_temp_f_bad_crc():
+    thermometers = fermbot.thermo.get_thermometers(BAD_CRC_THERMO_BUS_PATH)
+    with pytest.raises(fermbot.thermo.TempReadingError): #@UndefinedVariable
+        thermometers[0].temp_f
